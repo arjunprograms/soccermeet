54d6cf8ddf6d14affd0a5e6fef18879c18e30e58
arjunprograms <arjun.subedischool@gmail.com>
2025-05-01 20:54:00 -0700

    Use environment variables for Firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,25 +1,22 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Hard-coded Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC-CYThs7xpQv30clk0EyJ7eSBRZ5DfhTE",
-  authDomain: "soccermeet-1383e.firebaseapp.com",
-  projectId: "soccermeet-1383e",
-  storageBucket: "soccermeet-1383e.firebasestorage.app",
-  messagingSenderId: "610550831924",
-  appId: "1:610550831924:web:d7b02118e89cbb4a49e361",
-  measurementId: "G-0MWVXSB1BE"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
-// Initialize Firebase with try/catch
 let app, auth, db, storage;
 
 try {
-  console.log("Firebase initialization starting with hard-coded config");
+  console.log("Firebase initializing with ENV variables...");
   app = initializeApp(firebaseConfig);
   auth = getAuth(app);
   db = getFirestore(app);
